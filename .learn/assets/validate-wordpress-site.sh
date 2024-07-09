dd62923f4ce5a65f12b3c63eb14fdf403ffe4450
Alejandro Sanchez <aalejo@gmail.com>
2024-07-08 22:38:14 -0400

    Update validate-wordpress-site.sh

--- .learn/assets/validate-wordpress-site.sh
+++ .learn/assets/validate-wordpress-site.sh
@@ -6,26 +6,6 @@ mariadb_status=$(eval "systemctl status mariadb|grep active");
 
 site_data=$(eval "curl -s http://localhost/wordpress/index.php/wp-json/wp-site-health/v1");
 
-url=$1
-if [ -z $1 ];then
-  # URL de la petición POST (reemplaza con la URL de tu servidor)
-  echo "Ingrese la dirección de learnpack"
-  echo "Type learnpack address"
-  read url
-fi
-
 data='{"apacheStatus":"'$apache_status'","databaseStatus":"'$mariadb_status'","siteData":'$site_data'}'
 
-#echo $data
-
-cabeceras="Content-Type: application/json"
-# Enviar la petición POST usando curl
-curl -X POST --json "$data" -H "$cabeceras" $url
-
-# Verificar la respuesta del servidor
-if [ $? -ne 0 ]; then
-  echo "Error: La petición POST falló."
-  exit 1
-else
-  echo "Data enviada correctamente."
-fi
+echo "$data" > report.json
